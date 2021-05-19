ac6a6a1c9100e7c69fee0c1b6e6086735c95e70b
tomas-ondrejka <tomas.ondrejka2000@gmail.com>
2021-05-19 19:50:25 +0200

    DatabaseAccess updated. Now the connection to database works

--- src/server/database/DatabaseAccess.java
+++ src/server/database/DatabaseAccess.java
@@ -4,24 +4,15 @@ import java.sql.*;
 
 public class DatabaseAccess
 {
-  private String driver = "org.postgresql.Driver";
-  private String url = "jdbc:postgresql://hattie.db.elephantsql.com:5432/yunjxnkw";
+  private String url = "jdbc:postgresql://hattie.db.elephantsql.com:5432/yunjxnkw?currentSchema=dhms";
   private String user = "yunjxnkw";
   private String pw= "D5YTBHvbbrXKkABkXyFqK-L4jQDNVsBH";
 
   private static DatabaseAccess instance = new DatabaseAccess();
 
-  private DatabaseAccess()
-  {
-    try {
-      Class.forName(driver);
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    }
-  }
-
   public synchronized Connection getConnection(){
     try {
+      DriverManager.registerDriver(new org.postgresql.Driver());
       return DriverManager.getConnection(url,user,pw);
     } catch (SQLException e) {
       throw new RuntimeException("Failed to get DB connection!");
