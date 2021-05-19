cfe079cb5b7cae72e1afbb6dae65522720038d19
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-19 10:41:22 +0200

    server.database.manager has the methods in the interfaces

--- src/server/database/manager/EmployeeDBAccessManager.java
+++ src/server/database/manager/EmployeeDBAccessManager.java
@@ -1,5 +1,18 @@
 package server.database.manager;
 
+import shared.Doctor;
+import shared.Nurse;
+
+import java.util.ArrayList;
+
 public interface EmployeeDBAccessManager
 {
+    String addDoctor(Doctor doctor);
+    String addNurse(Nurse nurse);
+    String editDoctor(Doctor doctor);
+    String editNurse(Nurse nurse);
+    ArrayList<Doctor> getAllDoctors();
+    ArrayList<Nurse> getAllNurses();
+    String removeDoctor(Doctor doctor);
+    String removeNurse(Nurse nurse);
 }
