d26f1a78a1bdd86b6c54e6722ff6c7f7524d2da5
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-19 11:12:37 +0200

    shared.model.nurse methods implemented

--- src/server/model/nurse/AppointmentsServerModelNurseImpl.java
+++ src/server/model/nurse/AppointmentsServerModelNurseImpl.java
@@ -1,37 +1,42 @@
 package server.model.nurse;
 
+import server.database.nurse.AppointmentsDBAccessNurse;
+import server.database.nurse.AppointmentsDBAccessNurseImpl;
 import shared.Patient;
+
+import javax.net.ssl.SNIServerName;
+import javax.net.ssl.SSLContext;
 
 public class AppointmentsServerModelNurseImpl implements AppointmentsServerModelNurse
 {
-    // TODO: 19/05/2021 Implement methods
+    private AppointmentsDBAccessNurse dbAccessNurse;
 
     public AppointmentsServerModelNurseImpl()
     {
-
+        dbAccessNurse = new AppointmentsDBAccessNurseImpl();
     }
 
     @Override
     public void addPatient(Patient patient)
     {
-
+        dbAccessNurse.addPatient(patient);
     }
 
     @Override
     public void editPatient(Patient patient)
     {
-
+        dbAccessNurse.editPatient(patient);
     }
 
     @Override
     public void removePatient(Patient patient)
     {
-
+        dbAccessNurse.removePatient(patient);
     }
 
     @Override
-    public Patient getPatientBySNN(int SNN)
+    public Patient getPatientBySNN(int SSN)
     {
-        return null;
+        return dbAccessNurse.getPatientBySNN(SSN);
     }
 }
