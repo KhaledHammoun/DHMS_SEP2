8eab62a6314f757ba4d407a038c6b5d4f96a9907
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-19 10:42:21 +0200

    server.database.nurse has the methods in the interfaces

--- src/server/database/nurse/AppointmentsDBAccessNurse.java
+++ src/server/database/nurse/AppointmentsDBAccessNurse.java
@@ -1,5 +1,11 @@
 package server.database.nurse;
 
+import shared.Patient;
+
 public interface AppointmentsDBAccessNurse
 {
+    void addPatient(Patient patient);
+    void editPatient(Patient patient);
+    void removePatient(Patient patient);
+    Patient getPatientBySNN(int SNN);
 }
