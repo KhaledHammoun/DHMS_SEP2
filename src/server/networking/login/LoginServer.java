40315e08479c1e32183b41a97164d22adde06442
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-19 11:36:22 +0200

    shared.networking.login structure tree created methods not implemented. Have to decide if we make method for starting the RMIs

--- src/server/networking/login/LoginServer.java
+++ src/server/networking/login/LoginServer.java
@@ -1,5 +1,9 @@
 package server.networking.login;
 
+import shared.AccessType;
+import shared.LoginUser;
+
 public interface LoginServer
 {
+    AccessType login(LoginUser user);
 }
---
+++ src/server/networking/login/LoginServerRMI.java
@@ -0,0 +1,18 @@
+package server.networking.login;
+
+import shared.AccessType;
+import shared.LoginUser;
+
+public class LoginServerRMI implements LoginServer
+{
+    public LoginServerRMI()
+    {
+
+    }
+
+    @Override
+    public AccessType login(LoginUser user)
+    {
+        return null;
+    }
+}
