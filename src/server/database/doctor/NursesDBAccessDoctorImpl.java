7849a19f4afbc88c78e4ceaf57d423069bc21b65
tomas-ondrejka <tomas.ondrejka2000@gmail.com>
2021-05-20 23:11:53 +0200

    Creating test in NursesModelDoctor

--- src/server/database/doctor/NursesDBAccessDoctorImpl.java
+++ src/server/database/doctor/NursesDBAccessDoctorImpl.java
@@ -15,6 +15,35 @@ public class NursesDBAccessDoctorImpl implements NursesDBAccessDoctor
 {
   @Override public ArrayList<Nurse> getAllAvailableNurses()
   {
+    try (Connection connection = DatabaseAccess.getInstance().getConnection();
+        PreparedStatement preparedStatement = connection
+            .prepareStatement("SELECT * FROM nurse WHERE doctor_ssn is null"))
+    {
+      ResultSet r = preparedStatement.executeQuery();
+
+      ArrayList<Nurse> result = new ArrayList<>();
+
+      while (r.next())
+      {
+        Address address = new Address(r.getString("add_street"),
+            r.getString("add_no"), r.getString("add_zip_code"),
+            r.getString("add_city"));
+
+        result.add(new Nurse(r.getLong("ssn"), r.getLong("doctor_ssn"),
+            r.getString("f_name"), r.getString("mid_name"),
+            r.getString("l_name"), r.getDate("dob"), address,
+            r.getString("contact_f_name"), r.getString("contact_mid_name"),
+            r.getString("contact_l_name"), r.getString("contact_phone"),
+            r.getDate("start_date"), r.getString("education"),
+            r.getString("experience"), r.getString("email"),
+            r.getString("password")));
+      }
+      return result;
+    }
+    catch (SQLException e)
+    {
+      e.printStackTrace();
+    }
     return null;
   }
 
