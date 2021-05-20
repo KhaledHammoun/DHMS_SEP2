82473e2c4b0c779d142ad3b48a2b1926d25bd019
Tabita <304181@via.dk>
2021-05-20 14:48:52 +0200

    adding, editing samples views

---
+++ src/client/view/doctor/EditSampleController.java
@@ -0,0 +1,27 @@
+package client.view.doctor;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class EditSampleController
+{
+  @FXML
+  public TextField result;
+  @FXML
+  public Label typeLabel;
+  @FXML
+  public Label deadlineLabel;
+  @FXML
+  public Button saveButton;
+  @FXML
+  public ComboBox priority;
+
+  public void onSaveButton(ActionEvent actionEvent)
+  {
+    //TODO
+  }
+}
