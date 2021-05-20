85494718b568ec09a2d6e2804df2bf259eb17fa5
tomas-ondrejka <tomas.ondrejka2000@gmail.com>
2021-05-20 09:19:40 +0200

    Renaming Disease to Diagnosis, adding fields for dateFrom and dateTo

--- src/shared/Diagnosis.java
+++ src/shared/Diagnosis.java
@@ -1,18 +1,54 @@
 package shared;
 
 import java.io.Serializable;
+import java.sql.Date;
 
-public class Disease implements Serializable
+public class Diagnosis implements Serializable
 {
   private String name;
   private int severityLevel;
   private String description;
+  private int id;
+  private Date dateFrom;
+  private Date dateTo = null;
 
-  public Disease(String name, int severityLevel, String description)
+  public Diagnosis(int id, String name, int severityLevel, String description, Date dateFrom)
   {
+    this.id = id;
     this.name = name;
     this.severityLevel = severityLevel;
     this.description = description;
+    this.dateFrom = dateFrom;
+  }
+
+  public Date getDateTo()
+  {
+    return dateTo;
+  }
+
+  public void setDateTo(Date dateTo)
+  {
+    this.dateTo = dateTo;
+  }
+
+  public Date getDateFrom()
+  {
+    return dateFrom;
+  }
+
+  public void setDateFrom(Date dateFrom)
+  {
+    this.dateFrom = dateFrom;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public void setId(int id)
+  {
+    this.id = id;
   }
 
   public String getName()
