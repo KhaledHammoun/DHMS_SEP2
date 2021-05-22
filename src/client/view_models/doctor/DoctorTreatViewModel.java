aa8b40756af2942f437bb5b7ed630e1a24d49aae
Tabita <304181@via.dk>
2021-05-22 13:11:37 +0200

    mooving DoctorTreatViewModel to view models

--- src/client/view_models/doctor/DoctorTreatViewModel.java
+++ src/client/view_models/doctor/DoctorTreatViewModel.java
@@ -1,5 +1,53 @@
 package client.view_models.doctor;
 
+import client.model.doctor.TreatAndUpdateModelDoctor;
+import client.model.shared.GetPatientDataModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import shared.Diagnosis;
+
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+
 public class DoctorTreatViewModel
 {
+  private ObservableList<Diagnosis> diagnoses;
+  private StringProperty medication;
+  private StringProperty description;
+
+  private GetPatientDataModel getPatientDataModel;
+  private TreatAndUpdateModelDoctor treatAndUpdateModelDoctor;
+
+  public DoctorTreatViewModel(TreatAndUpdateModelDoctor treatAndUpdateModelDoctor, GetPatientDataModel getPatientDataModel)
+  {
+    this.getPatientDataModel = getPatientDataModel;
+    this.treatAndUpdateModelDoctor = treatAndUpdateModelDoctor;
+    medication = new SimpleStringProperty();
+    description = new SimpleStringProperty();
+    //ToDo implement observer
+    //treatAndUpdateModelDoctor.addListener("AllDiagnosis", this::loadDiagnoses);
+    diagnoses = FXCollections.observableArrayList();
+
+  }
+
+  public void loadDiagnoses(PropertyChangeEvent evt)
+  {
+    diagnoses.addAll((ArrayList<Diagnosis>) evt.getNewValue());
+  }
+  public ObservableList<Diagnosis> getDiagnoses()
+  {
+    return diagnoses;
+  }
+
+  public StringProperty medicationProperty()
+  {
+    return medication;
+  }
+
+  public StringProperty descriptionProperty()
+  {
+    return description;
+  }
 }
