4374150f986c7904f171309f6146b5a9c933e6cb
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-22 19:21:34 +0200

    Can login and select a option from the main view of each employee

--- src/client/view/login/ManagerMainViewController.java
+++ src/client/view/login/ManagerMainViewController.java
@@ -2,8 +2,9 @@ package client.view.login;
 
 import client.core.ViewHandler;
 import client.core.ViewModelFactory;
+import client.view.View;
 import client.view.ViewController;
-import javafx.event.ActionEvent;
+import client.view_models.login.ManagerMainViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -12,24 +13,32 @@ public class ManagerMainViewController implements ViewController
     @FXML
     private Label loggedInAsLabel;
 
+    private ViewHandler viewHandler;
+    private ManagerMainViewModel managerMainViewModel;
+
     @FXML
-    public void onEmployeeButton(ActionEvent actionEvent)
+    public void onEmployeeButton()
     {
+        viewHandler.openView(View.EMPLOYEE);
     }
 
     @FXML
-    public void onWardsButton(ActionEvent actionEvent)
+    public void onWardsButton()
     {
+        viewHandler.openView(View.WARD);
     }
 
     @FXML
-    public void onLogoutButton(ActionEvent actionEvent)
+    public void onLogoutButton()
     {
+        managerMainViewModel.logout();
+        viewHandler.openView(View.LOGIN);
     }
 
     @Override
     public void init(ViewModelFactory viewModelFactory, ViewHandler viewHandler)
     {
-
+        this.viewHandler = viewHandler;
+        this.managerMainViewModel = (ManagerMainViewModel) viewModelFactory.getViewModel(View.MANAGER_MAIN);
     }
 }
--- src/client/view/login/NurseMainViewController.java
+++ src/client/view/login/NurseMainViewController.java
@@ -2,8 +2,9 @@ package client.view.login;
 
 import client.core.ViewHandler;
 import client.core.ViewModelFactory;
+import client.view.View;
 import client.view.ViewController;
-import javafx.event.ActionEvent;
+import client.view_models.login.NurseMainViewModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,30 +12,39 @@ public class NurseMainViewController implements ViewController
 {
     @FXML
     private Label loggedInAsLabel;
+    private ViewHandler viewHandler;
+    private NurseMainViewModel nurseMainViewModel;
 
     @FXML
-    public void onPatientsButton(ActionEvent actionEvent)
+    public void onPatientsButton()
     {
+        viewHandler.openView(View.ALL_APPOINTMENTS);
     }
 
     @FXML
-    public void onAddPatientButton(ActionEvent actionEvent)
+    public void onAddPatientButton()
     {
+        viewHandler.openView(View.ADD_PATIENT);
+
     }
 
     @FXML
-    public void onAppointmentsButton(ActionEvent actionEvent)
+    public void onAppointmentsButton()
     {
+        viewHandler.openView(View.MAKE_APPOINTMENT);
     }
 
     @FXML
-    public void onLogoutButton(ActionEvent actionEvent)
+    public void onLogoutButton()
     {
+        nurseMainViewModel.logout();
+        viewHandler.openView(View.LOGIN);
     }
 
     @Override
     public void init(ViewModelFactory viewModelFactory, ViewHandler viewHandler)
     {
-
+        this.viewHandler = viewHandler;
+        this.nurseMainViewModel = (NurseMainViewModel) viewModelFactory.getViewModel(View.NURSE_MAIN);
     }
 }
