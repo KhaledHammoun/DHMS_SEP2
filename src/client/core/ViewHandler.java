b78c8b053ce87675f145f36a7fe38fc8ffeb9668
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-22 20:20:03 +0200

    icon to the system added

--- src/client/core/ViewHandler.java
+++ src/client/core/ViewHandler.java
@@ -2,6 +2,7 @@ package client.core;
 
 import client.view.View;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.concurrent.locks.Lock;
@@ -42,8 +43,9 @@ public class ViewHandler
     public void openView(View view)
     {
         Scene scene = ViewFactory.getScene(view);
-        stage.setTitle(view.name());
         stage.setScene(scene);
+        Image image = new Image(getClass().getResourceAsStream("../images/image.png"));
+        stage.getIcons().add(image);
         stage.setResizable(false);
         stage.show();
     }
