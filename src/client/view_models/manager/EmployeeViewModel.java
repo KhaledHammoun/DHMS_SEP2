6c64153433757cf0ffe7a91a1cc4ecd8aeb8d687
Khaled Hammoun <kh.hammoun@gmail.com>
2021-05-23 10:39:05 +0200

    manager employee view implemented. Can get list of all employees and request add or edit. We dont have method for removing employees.

--- src/client/view_models/manager/EmployeeViewModel.java
+++ src/client/view_models/manager/EmployeeViewModel.java
@@ -1,6 +1,7 @@
 package client.view_models.manager;
 
 import client.model.manager.EmployeeModelManager;
+import client.model.shared.GetEmployeeDataModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import shared.Employee;
@@ -11,23 +12,46 @@ public class EmployeeViewModel
 {
     private ObservableList<String> employeeType;
     private ObservableList<Employee> employees;
-    private EmployeeModelManager modelManager;
+    private EmployeeModelManager employeeModelManager;
+    private GetEmployeeDataModel getEmployeeDataModel;
 
-    public EmployeeViewModel(Object modelManager)
+    public EmployeeViewModel(Object employeeModelManager, Object getEmployeeDataModel)
     {
-        this.modelManager = (EmployeeModelManager) modelManager;
+        this.getEmployeeDataModel = (GetEmployeeDataModel) getEmployeeDataModel;
+        this.employeeModelManager = (EmployeeModelManager) employeeModelManager;
         employeeType = FXCollections.observableArrayList("Doctor", "Nurse");
         employees = FXCollections.observableArrayList();
     }
 
-    public ObservableList<Employee> getEmployees()
+    public ObservableList<Employee> employeesProperty()
     {
         return employees;
     }
 
-    public void setEmployees(ArrayList<Employee> employees)
+    public <T extends Employee> void setEmployees(ArrayList<T> employees)
     {
+        this.employees.clear();
         this.employees.addAll(employees);
+    }
+
+    public void getListOfDoctors()
+    {
+        setEmployees(getEmployeeDataModel.getListOfAllDoctors());
+    }
+
+    public void getListOfNurses()
+    {
+        setEmployees(getEmployeeDataModel.getListOfAllNurses());
+    }
+
+    public void editDoctor(Long ssn)
+    {
+        getEmployeeDataModel.getDoctorBySSN(ssn);
+    }
+
+    public void editNurse(Long ssn)
+    {
+        getEmployeeDataModel.getNurseBySSN(ssn);
     }
 
     public void addEmployee(Employee employee)
